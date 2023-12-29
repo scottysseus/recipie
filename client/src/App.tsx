ee5a56b2614800f4fd1f27f23048fb77d4cf6c4b
scottysseus <sweidenkopf@gmail.com>
2023-12-29 16:47:39 -0500

    testing pre-commit hook

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,12 @@
-import { createSignal } from "solid-js";
+// import { createSignal } from "solid-js";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = createSignal(0);
+  // const [count, setCount] = createSignal(0);
 
   return (
-    <>   
-        <label>Recipe URL</label>
+    <>
+      <label>Recipe URL</label>
       <input placeholder="www.chef.com/cookie-recipe"></input>
     </>
   );
