68b8997ffa774fda15629a8eabc8f8fd4cb8b1af
scottysseus <sweidenkopf@gmail.com>
2024-01-27 17:19:17 -0500

    fixed ol and ul in the recipe page

--- client/src/Recipe.tsx
+++ client/src/Recipe.tsx
@@ -11,28 +11,24 @@ export function Recipe({ recipe }: { recipe: RecipeRecord }) {
         From {toLocalizedDateTimeString(recipe.created)}
       </p>
       <h2 class="mb-6 mt-6">Ingredients</h2>
-      <ul>
+      <ul class="list-disc">
         <For each={recipe.ingredients}>
           {(ingredient: Ingredient) => (
-            <li class="text-sm">
+            <li class="ml-6 text-sm">
               {ingredient.name}, {ingredient.quantity} {ingredient.unit}
             </li>
           )}
         </For>
       </ul>
       <h2 class="mb-6 mt-6">Instructions</h2>
-      {/* {
-        (recipe.instructions as unknown as { instructions: string[] })
-          .instructions
-      } */}
-      <ol>
+      <ol class="list-decimal">
         <For
           each={
             (recipe.instructions as unknown as { instructions: string[] })
               .instructions
           }
         >
-          {(instruction: string) => <li class="text-sm">{instruction}</li>}
+          {(instruction: string) => <li class="ml-6 text-sm">{instruction}</li>}
         </For>
       </ol>
     </>
