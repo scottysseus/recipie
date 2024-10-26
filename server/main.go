797bde4aad6f92f08a872476cd8e44fd0f4cd1c8
scottysseus <sweidenkopf@gmail.com>
2024-10-25 21:37:12 -0400

    added allowed users list check before sign up, and auto-add admins to the allwoed users list

--- server/main.go
+++ server/main.go
@@ -7,6 +7,7 @@ import (
 	"github.com/pocketbase/pocketbase"
 	"github.com/pocketbase/pocketbase/apis"
 	"github.com/pocketbase/pocketbase/core"
+	"github.com/pocketbase/pocketbase/models"
 	"github.com/pocketbase/pocketbase/plugins/migratecmd"
 
 	_ "github.com/scottysseus/recipie/migrations"
@@ -22,6 +23,18 @@ func main() {
 
 	worker := NewSmartImportWorker(app)
 
+	app.OnAdminAfterCreateRequest().Add(func(e *core.AdminCreateEvent) error {
+		allowedUsersCollection, err := app.Dao().FindCollectionByNameOrId("allowedUsers")
+		if err != nil {
+			return err
+		}
+
+		newAllowedUserRecord := models.NewRecord(allowedUsersCollection)
+		newAllowedUserRecord.Set("email", e.Admin.Email)
+
+		return app.Dao().SaveRecord(newAllowedUserRecord)
+	})
+
 	app.OnRecordBeforeAuthWithOAuth2Request().Add(func(e *core.RecordAuthWithOAuth2Event) error {
 		if e.OAuth2User.Email == "" {
 			return apis.NewForbiddenError("a valid email address is required to sign up", struct {
@@ -29,14 +42,15 @@ func main() {
 			}{id: e.OAuth2User.Id})
 		}
 
-		var count struct{ count int }
+
+		var count struct{ Count int `db:"count"`}
 		err := app.Dao().DB().NewQuery("SELECT COUNT(*) as count FROM allowedUsers WHERE email = {:email}").Bind(dbx.Params{"email": e.OAuth2User.Email}).One(&count)
 		if err != nil {
 			app.Logger().Error("err accessing the allowlist", "err", err)
 			return apis.NewApiError(500, "error accessing the allowlist", nil)
 		}
 
-		if count.count < 1 {
+		if count.Count < 1 {
 			return apis.NewUnauthorizedError("enrollment in this application is restricted to allowlisted users", struct{ email string }{email: e.OAuth2User.Email})
 		}
 
